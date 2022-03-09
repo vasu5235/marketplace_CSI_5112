da928b2a3f6d8e55d79bddadf4a31a6d6e1c406e
Bhavesh Singh <bhaveshsingh0124@gmail.com>
2022-03-08 22:06:54 -0500

    product add to cart feature

---
+++ frontend/marketplace/lib/utils/cart_products_controller.dart
@@ -0,0 +1,45 @@
+class CartProductsController {
+  static var cartProducts = [
+    {
+      'name': 'iPhone 123',
+      'image': 'images/product_images/iphone.jpg',
+      'price': 400
+    },
+    {
+      'name': 'iPhone 13',
+      'image': 'images/product_images/iphone.jpg',
+      'price': 400
+    },
+  ];
+
+  CartProductsController() {}
+
+  getProducts() {
+    return cartProducts;
+  }
+
+  // addProductToCart(int _id, String _name, String _imageURL, String _description,
+  //     String _category, double _price) {
+  //   cartProducts.add({
+  //     'id': _id,
+  //     'name': _name,
+  //     'image': _imageURL,
+  //     'description': _description,
+  //     'category': _category,
+  //     'price': _price
+  //   });
+  // }
+
+  addProductToCart(String _name, String _imageURL, double _price) {
+    cartProducts.add({'name': _name, 'image': _imageURL, 'price': _price});
+  }
+
+  clearCart() {
+    cartProducts = [];
+  }
+
+  //todo: remove product using productId
+  void removeProductFromCart(String productName) {
+    cartProducts.removeWhere((product) => product["name"] == productName);
+  }
+}
