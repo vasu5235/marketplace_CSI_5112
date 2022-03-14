432dd633b8f48f02180894e22dbda2e98aea3789
Vasu Mistry <vasu5235@gmail.com>
2022-03-14 12:31:06 -0400

    added envURL variable for ease of configuration deployment

--- frontend/marketplace/lib/constants/api_url.dart
+++ frontend/marketplace/lib/constants/api_url.dart
@@ -1,7 +1,7 @@
 class ApiUrl {
   static const String envUrl = "https://localhost:7136/api";
   //API calls related constants
-  static const String get_category = 'https://localhost:7136/api/Category';
-  static const String get_recent_product = 'https://localhost:7136/api/Product';
-  static const String get_product_by_id = 'https://localhost:7136/api/Product/';
+  static const String get_category = envUrl + '/Category';
+  static const String get_recent_product = envUrl + '/Product';
+  static const String get_product_by_id = envUrl + '/Product/';
 }
